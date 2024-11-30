3f2e9b58fb6559883cfccb514bb04d0ed3bb36b9
Oleg-Ivnv <oleg.ivanov@waites.net>
2024-11-30 06:54:35 +0200

    refactor: remake string parse function

--- src/helpers/parseStringWithVars.ts
+++ src/helpers/parseStringWithVars.ts
@@ -1,24 +1,37 @@
 import { Answers } from "@/store/slices/questionnaireSlice";
 
+const isItemDefaultString = (item: string) =>
+  item[0] !== "{" || item[item.length - 1] !== "}";
+
+const isConditionalExecutable = (executable: string) => executable[0] === "!";
+
+const getExecutable = (item: string) => item.replace("{", "").replace("}", "");
+
+const executeConditional = (executable: string, vars: Answers) => {
+  const cleared = executable.replace("!", "");
+
+  const [val, display] = cleared.split("(");
+
+  return vars[+val] ? display.replace(")", "").replaceAll("_", " ") : "";
+};
+
+const execute = (executable: string, vars: Answers) => vars[+executable] ?? "";
+
 export const parseStringWithVars = (str: string, vars: Answers): string => {
   return str
     .split(" ")
     .map((item) => {
-      if (item[0] !== "{" || item[item.length - 1] !== "}") {
+      if (isItemDefaultString(item)) {
         return item;
       }
 
-      const executable = item.replace("{", "").replace("}", "");
+      const executable = getExecutable(item);
 
-      if (executable[0] === "!") {
-        const cleared = executable.replace("!", "");
+      const executeFn = isConditionalExecutable(executable)
+        ? executeConditional
+        : execute;
 
-        const [val, display] = cleared.split("(");
-
-        return vars[+val] ? display.replace(")", "").replaceAll("_", " ") : "";
-      }
-
-      return vars[+executable] ?? "";
+      return executeFn(executable, vars);
     })
     .join(" ");
 };
